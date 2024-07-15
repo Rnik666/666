deb28bd24758e0679eaafe4e250ecd9cc160f07f
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-07-15 12:39:26 +0800

    更新 LY1.js

--- LY1.js
+++ LY1.js
@@ -1,6 +1,6 @@
 let body = $response.body;
 body = body.replace(/("isVip"\s*:\s*)\w+/g, '$1true');
-body = body.replace(/("vipExpire"\s*:\s*)\d+/g, '$14092610661000');
+body = body.replace(/("vipExpire"\s*:\s*)\d+/g, '$14092599349');
 body = body.replace(/("name"\s*:\s*")[^"]*"/g, '$1Rnik666🎖"');
 body = body.replace(/("amount"\s*:\s*")[^"]*"/g, '$1https://pavo.elongstatic.com/i/ori/1uG8Yb8CUWA.png"');
 
