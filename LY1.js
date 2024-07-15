399612559a3e6f7e9de6b4684f0c26eca658a499
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-07-15 12:36:53 +0800

    更新 LY1.js

--- LY1.js
+++ LY1.js
@@ -1,6 +1,7 @@
-var Rnik = JSON.parse($response.body);
-  Rnik.data.user.vipExpire = 4092599349;
-Rnik.data.user.isVip = true;
-Rnik.data.user.avatar = "https://file.shengmaapp.com/storage/assistant/20240610/50e92adb5462bac2d17a17bc9c8298eb.jpg";
-Rnik.data.user.name = "Rnik666🎖";
-  $done({body : JSON.stringify(Rnik)});
+let body = $response.body;
+body = body.replace(/("isVip"\s*:\s*)\w+/g, '$1true');
+body = body.replace(/("vipExpire"\s*:\s*)\d+/g, '$14092610661000');
+body = body.replace(/("name"\s*:\s*")[^"]*"/g, '$1Rnik666🎖"');
+body = body.replace(/("amount"\s*:\s*")[^"]*"/g, '$1https://pavo.elongstatic.com/i/ori/1uG8Yb8CUWA.png"');
+
+$done({ body });
